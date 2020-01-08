b33ee6b9f0f4927620d61b69645bf37ce25a886e
caio myra pereira <caiomyrapereira@hotmail.com>
2020-01-07 22:20:50 -0300

    init project

---
+++ project/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class App extends React.Component{
+  
+  constructor(){
+    super();
+    this.state = {
+
+    };
+  }
+
+  render(){
+    return (
+      <div className="App">
+        My app 
+      </div>
+    );
+  }
+}
+
+export default App;
